f91b116d31d98c81986d23acf151293e41232c0d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2018-01-25 17:00:27 +0000

    Add spacing

--- run_tests.bat
+++ run_tests.bat
@@ -1,6 +1,6 @@
 setlocal
 call C:\Instrument\Apps\EPICS\config_env.bat
-start/wait cmd/c C:\Instrument\Apps\EPICS\start_ibex_server.bat
+start /wait cmd /c C:\Instrument\Apps\EPICS\start_ibex_server.bat
 %PYTHON% %~dp0run_tests.py || echo "running tests failed."
 call C:\Instrument\Apps\EPICS\ISIS\JournalParser\master\test\run_tests.bat || echo "running tests failed."
-start/wait cmd/c C:\Instrument\Apps\EPICS\stop_ibex_server.bat
+start /wait cmd /c C:\Instrument\Apps\EPICS\stop_ibex_server.bat
