7c43a62ec9553623f57f2e6f42d98135629bd1c3
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-07-17 16:21:52 +0100

    Make tests more robust

--- test_plotting.py
+++ test_plotting.py
@@ -1,5 +1,5 @@
+import os
 import unittest
-
 from utilities.utilities import g
 from six.moves import range
 import requests
@@ -29,6 +29,7 @@ class TestPlotting(unittest.TestCase):
 
         THE ORDER OF THESE ITEMS IS IMPORTANT!
         """
+        g.set_instrument(os.getenv("MYPVPREFIX"))
         import matplotlib
         matplotlib.rcParams['backend'] = "module://genie_python.matplotlib_backends/ibex_web_backend"
         import matplotlib.backends
@@ -40,15 +41,29 @@ class TestPlotting(unittest.TestCase):
     def setUp(self):
         TestPlotting.PYPLOT.close('all')
 
-    def test_GIVEN_spectra_plot_THEN_no_exceptions_thrown(self):
-        g.plot_spectrum(1)
-        time.sleep(5)  # Ensure webserver has time to start
+    def assert_webserver_up(self):
         web_response = requests.get("http://127.0.0.1:8988/")
         self.assertEqual(web_response.status_code, 200)
+
+    def test_GIVEN_spectra_plot_THEN_no_exceptions_thrown(self):
+        g.begin()
+        try:
+            g.plot_spectrum(1)
+            self.assert_webserver_up()
+        finally:
+            g.end()
+
+    def test_GIVEN_spectra_plot_WHEN_adding_more_traces_THEN_no_exceptions_thrown(self):
+        g.begin()
+        try:
+            p = g.plot_spectrum(1)
+            p.add_spectrum(2)
+            p.add_spectrum(3, period=2)
+            self.assert_webserver_up()
+        finally:
+            g.end()
 
     def test_GIVEN_when_plot_exists_WHEN_connect_to_matplotlib_server_THEN_response_is_http_200_ok(self):
         TestPlotting.PYPLOT.plot(range(5))
         TestPlotting.PYPLOT.show()
-        time.sleep(5)  # Ensure webserver has time to start
-        web_response = requests.get("http://127.0.0.1:8988/")
-        self.assertEqual(web_response.status_code, 200)
+        self.assert_webserver_up()
