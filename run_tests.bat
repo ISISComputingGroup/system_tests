b98b01ea052d0515b22cbbd31cdd9446445dda57
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2019-09-24 14:51:02 +0100

    Set PYTHONUNBUFFERED

--- run_tests.bat
+++ run_tests.bat
@@ -1,6 +1,7 @@
 setlocal
 call C:\Instrument\Apps\EPICS\config_env.bat
 start /wait cmd /c C:\Instrument\Apps\EPICS\start_ibex_server.bat
+set "PYTHONUNBUFFERED=1"
 %PYTHON% "%~dp0run_tests.py" || echo "running tests failed."
 call C:\Instrument\Apps\EPICS\ISIS\JournalParser\master\test\run_tests.bat || echo "running tests failed."
 start /wait cmd /c C:\Instrument\Apps\EPICS\stop_ibex_server.bat
