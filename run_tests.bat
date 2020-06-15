2976c1914edcbaf0d3b98c29b458d70d8f848094
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-06-15 16:54:04 +0100

    Check exit codes

--- run_tests.bat
+++ run_tests.bat
@@ -4,7 +4,22 @@ python test_setup_teardown.py>base_line_memory.txt
 set /P BASE_MEMORY_USAGE=<base_line_memory.txt
 start /wait cmd /c %EPICS_ROOT%\start_ibex_server.bat
 set "PYTHONUNBUFFERED=1"
-python "%~dp0run_tests.py" %* || echo "running base tests failed."
-call %EPICS_ROOT%\ISIS\JournalParser\master\test\run_tests.bat || echo "running journal tests failed."
-start /wait cmd /c %EPICS_ROOT%\stop_ibex_server.bat
+set "exitcode=0"
+python "%~dp0run_tests.py" %*
+IF %errorlevel% NEQ 0 (
+	set exitcode=%errorlevel%
+    echo "running base tests failed."
+	goto finish
+)
+
+call %EPICS_ROOT%\ISIS\JournalParser\master\test\run_tests.bat
+IF %errorlevel% NEQ 0 (
+	set exitcode=%errorlevel%
+    echo "running journal tests failed."
+	goto finish
+)
+
+:finish
 python test_setup_teardown.py --tear_down
+start /wait cmd /c %EPICS_ROOT%\stop_ibex_server.bat
+EXIT /b %exitcode%
