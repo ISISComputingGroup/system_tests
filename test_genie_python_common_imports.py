28b393d3ac5328c444875df6618b747cbae1df28
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-09-25 21:23:00 +0100

    Don't import pip

--- test_genie_python_common_imports.py
+++ test_genie_python_common_imports.py
@@ -9,6 +9,19 @@ IGNORED_MODULES = {
     "dockerpty",  # Not supported on windows
     "adodbapi",  # Not needed by users
     "black",  # Not needed by users
+    # importing pip overwrites distutils in sys.modules
+    # with a copy of setuptools instead, which then breaks
+    # any packages that depend on distutils directly.
+    # https://github.com/pypa/pip/issues/8761
+    #
+    # Importing pip as a module in general is a bad idea (tm), so just don't do that.
+    #
+    # It's possible to use:
+    # >>> import _distutils_hack
+    # >>> _distutils_hack.remove_shim()
+    # >>> import pip
+    # if someone *really* needs to import pip (yes, seriously; no, it's not a good idea).
+    "pip",
 }
 
 
@@ -17,7 +30,7 @@ class TestGeniePythonImports(unittest.TestCase):
     Tests that modules which users use can be imported.
     """
 
-    def _attempt_to_import_module_by_name(self, module_name):
+    def _attempt_to_import_module_by_name(self, module_name: str) -> str | None:
         """
         Attempts to import a module by name.
         :param module_name: the module name to import
@@ -38,12 +51,13 @@ class TestGeniePythonImports(unittest.TestCase):
         else:
             return None
 
-    def test_WHEN_importing_all_installed_packages_THEN_no_error(self):
+    def test_WHEN_importing_all_installed_packages_THEN_no_error(self) -> None:
         """
         This tests that all of the modules we've installed are importable as modules.
         """
 
-        # Ignore warnings. We get lots of these from various modules and it's too noisy for this test suite.
+        # Ignore warnings. We get lots of these from various modules and it's too noisy for this
+        # test suite.
         warnings.filterwarnings("ignore")
 
         failures = []
