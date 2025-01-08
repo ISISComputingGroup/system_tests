af53af6dba36ea0d3aa9a2eee8571e5f290677de
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-01-08 00:50:53 +0000

    Print EPICS root name

--- create_virtual_env.bat
+++ create_virtual_env.bat
@@ -1,5 +1,7 @@
 if "%ICP_CONFIG_ENV_RUN%" == "" (
     call C:\Instrument\Apps\EPICS\config_env.bat
+) else (
+    @echo Using existing EPICS_ROOT %EPICS_ROOT%
 )
 del /q /s my_venv >NUL 2>&1
 %PYTHON3% -m venv --system-site-packages my_venv
