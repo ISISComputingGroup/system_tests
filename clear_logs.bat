4d2097b96e6033febcd652dcfed5aee1def2a1cc
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-03-08 12:05:04 +0000

    Update clear_logs.bat

--- clear_logs.bat
+++ clear_logs.bat
@@ -1,14 +1,5 @@
 setlocal
 pushd C:\Instrument\Var\logs
-del /s /q *.log
-del /q ioc\*.*
-del /q conserver\*.*
-del /q gateway\blockserver\*.*
-del /q gateway\external\*.*
-del /q genie_python\*.*
-del /q deploy\*.*
-del /q ibex_server\*.*
-del /q IOCTestFramework\*.*
 del /q /s *.*
 popd
 exit /b 0
