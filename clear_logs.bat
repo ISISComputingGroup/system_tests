32673c5c8de197fa5c15b89cd900bc33cc823229
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2026-06-19 12:36:03 +0100

    Suppress error messages in clear_logs.bat
    Redirect error messages to null while deleting files.

--- clear_logs.bat
+++ clear_logs.bat
@@ -1,9 +1,10 @@
 setlocal
-for /d %%i in ( C:\Instrument\Var\logs C:\Instrument\Var\autosave ) do (
+REM ignore errors as del may attempt to delete a directory
+for /d %%i in ( C:\Instrument\Var\logs C:\Instrument\Var\autosave C:\Instrument\Var\logs ) do (
     @echo Cleaning %%i
     if exist "%%i" (
         pushd %%i
-        del /s /q *.* >NUL
+        del /s /q *.* >NUL 2>&1
         popd
     )
 )  
